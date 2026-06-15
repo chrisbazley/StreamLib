9caf522d238f970038d107120ef9f1ef119f12c5
Christopher Bazley <cs99cjb@gmail.com>
2026-06-15 12:50:54 +0100

    Send the debug log to stderr not stdout

--- Internal/StreamMisc.h
+++ Internal/StreamMisc.h
@@ -23,6 +23,7 @@ History:
   CJB: 26-Aug-19: Now requires "PseudoFlex.h" too.
   CJB: 07-Jun-20: Added support for verbose debugging output.
   CJB: 09-Apr-25: Dogfooding the _Optional qualifier.
+  CJB: 15-Jun-26: Send the debug log to stderr not stdout.
 */
 
 #ifndef StreamMisc_h
@@ -44,15 +45,15 @@ History:
 #include <assert.h>
 
 #if defined(DEBUG_VERBOSE_OUTPUT) && defined (DEBUG_OUTPUT)
-#define DEBUG_VERBOSEF if (1) printf
+#define DEBUG_VERBOSEF(...) if (1) fprintf(stderr, __VA_ARGS__)
 #else
-#define DEBUG_VERBOSEF if (0) printf
+#define DEBUG_VERBOSEF(...) if (0) fprintf(stderr, __VA_ARGS__)
 #endif /* DEBUG_VERBOSE_OUTPUT && DEBUG_OUTPUT */
 
 #ifdef DEBUG_OUTPUT
-#define DEBUGF if (1) printf
+#define DEBUGF(...) if (1) fprintf(stderr, __VA_ARGS__)
 #else
-#define DEBUGF if (0) printf
+#define DEBUGF(...) if (0) fprintf(stderr, __VA_ARGS__)
 #endif /* DEBUG_OUTPUT */
 
 #endif /* USE_CBDEBUG */
@@ -75,6 +76,7 @@ static inline _Optional void *optional_malloc(size_t n)
     return malloc(n);
 }
 #undef malloc
+
 #define malloc(n) optional_malloc(n)
 
 static inline _Optional void *optional_realloc(_Optional void *p, size_t n)
